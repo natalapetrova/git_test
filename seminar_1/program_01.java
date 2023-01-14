eb7c78fff1b40315be87623ddff545bad5785c11
natali_petrova <natalapetrova@bk.ru>
2023-01-14 15:46:41 +0300

    add seminar 1 program 1

---
+++ seminar_1/program_01.java
@@ -0,0 +1,9 @@
+package seminar_1;
+
+import java.time.LocalDateTime;
+
+public class program_01 {
+    public static void main(String[] args) {
+        System.out.println(LocalDateTime.now());
+    }
+}
